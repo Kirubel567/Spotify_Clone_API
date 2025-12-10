f9efae782fda694019685ad480cbb025d80591fc
Kirubel <kirubel.wubet1996@gmail.com>
2025-12-11 01:42:16 +0300

    update artist

--- src/controllers/artistController.js
+++ src/controllers/artistController.js
@@ -4,6 +4,7 @@ const Artist = require("../models/Artist");
 const Song = require("../models/Song");
 const Album = require("../models/Album");
 const { uploadToCloudinary } = require("../utils/cloudinaryUploads");
+const { get } = require("mongoose");
 //@desc - create a new artist
 //@route - POST /api/artist
 //@Access - private
@@ -40,19 +41,88 @@ const createArtist = asyncHandler(async (req, res) => {
     isVerified: true,
     image: imageUrl,
   });
-  res.status(StatusCodes.CREATED).json(artists); 
+  res.status(StatusCodes.CREATED).json(artists);
 });
 
 //@desc - Get all artists with filtering and pagination
-//@route - GET /api/artist?genre=Rock&search=pink&page=1&limit=10
+//@route - GET /api/artist?genre=Rock&search=pink&page=1 &limit=10
 //@Access - public
 
-const getArtists = asyncHandler(async(req, res)=>{
-  const {genre, search, page=1, limit=10} = req.query; 
+const getArtists = asyncHandler(async (req, res) => {
+  const { genre, search, page = 1, limit = 10 } = req.query;
   //build filter object
-  
-})
+  const filter = {}; //filter object to filter query from db
+  if (genre) filter.genres = { $in: [genre] };
+  if (search) {
+    filter.$or = [
+      { name: { $regex: search, $options: "i" } },
+      { bio: { $regex: search, $options: "i" } },
+    ];
+  }
+  //Count total artists with filter
+  const count = await Artist.countDocuments(filter);
+  //pagination
+  const skip = (parseInt(page) - 1) * parseInt(limit);
+  //get the artist
+  const artists = await Artist.find(filter)
+    .sort({ followers: -1 })
+    .limit(parseInt(limit))
+    .skip(skip);
+
+  res.status(StatusCodes.OK).json({
+    artists,
+    page: parseInt(page),
+    pages: Math.ceil(count / parseInt(limit)),
+    totalArtist: count,
+  });
+});
+
+//@desc - Get a specific artist usin id
+//@route - GET /api/artist/id
+//@Access - public
+
+const getArtistsById = asyncHandler(async (req, res) => {
+  //get the specific artist with that id
+  const artist = await Artist.findById(req.params.id);
+  if (artist) {
+    res.status(StatusCodes.OK).json(artist);
+  } else {
+    res.status(StatusCodes.NOT_FOUND);
+    throw new Error("Artist not found");
+  }
+});
+
+//@desc - Update Artist Details
+//@route - GET /api/artist/id
+//@Access - private/Admin
+
+const updatedArtist = asyncHandler(async (req, res) => {
+  const { name, bio, genres, isVerified } = req.body;
+  const artist = await Artist.findById(req.params.id);
+  if (!artist) {
+    res.status(StatusCodes.NOT_FOUND);
+    throw new Error("Artist not found");
+  }
+  // update artist details
+  artist.name = name || artist.name;
+  artist.bio = bio || artist.bio;
+  artist.genres = genres || artist.genres;
+  artist.isVerified =
+    isVerified !== undefined ? isVerified === "true" : artist.isVerified;
+
+  //update the image if provided
+  if (req.file) {
+    const result = await uploadToCloudinary(req.file.path, "spotify/artists");
+    artist.image = result.secure_url;
+  }
+  //resave
+  const updatedArtist = await artist.save();
+  res.status(StatusCodes.OK).json(updatedArtist);
+});
+
 module.exports = {
-    createArtist, 
-    getArtists
-}
+  createArtist,
+  getArtists,
+  getArtistsById,
+  updatedArtist,
+};
--- src/routes/artistRoutes.js
+++ src/routes/artistRoutes.js
@@ -1,12 +1,25 @@
 const express = require("express");
-const { createArtist, getArtists } = require("../controllers/artistController");
+const {
+  createArtist,
+  getArtists,
+  getArtistsById,
+  updatedArtist,
+} = require("../controllers/artistController");
 const { protect, isAdmin } = require("../middlewares/auth");
 const upload = require("../middlewares/upload");
 
 const artistRouter = express.Router();
 // public routes
-artistRouter.get("/",getArtists);
+artistRouter.get("/", getArtists);
+artistRouter.get("/:id", getArtistsById);
 // Admin routes
 artistRouter.post("/", protect, isAdmin, upload.single("image"), createArtist);
+artistRouter.put(
+  "/:id",
+  protect,
+  isAdmin,
+  upload.single("image"),
+  updatedArtist
+);
 
 module.exports = artistRouter;
